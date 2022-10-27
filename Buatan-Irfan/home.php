34d78ee2b1e00b908888474f69e0420300e08460
Irfan-Taufiqurrahman <irfan.rachman72@gmail.com>
2022-10-27 20:13:34 +0700

    first commit

--- Buatan-Irfan/home.php
+++ Buatan-Irfan/home.php
@@ -17,53 +17,53 @@
 <body>
     <!-- navbar -->
     <data></data>iv id="header" class="text-white">
-        <div class="container">
-            <div class="navbar navbar-expand-md navbar-dark bg-transparent">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="Gallery/Grissee.png"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link fas fa-shopping-cart" href="#"> Shop</a>
-                            </li>
-                        </ul>
+    <div class="container">
+        <div class="navbar navbar-expand-md navbar-dark bg-transparent">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="Gallery/Grissee.png"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link fas fa-shopping-cart" href="#"> Shop</a>
+                        </li>
+                    </ul>
 
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Profile
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Settings
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Activity Log
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Logout
-                            </a>
-                        </div>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Settings
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Activity Log
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <section id="landingpage" class="text-center">
-                <p style="font-size: 6rem;">Center souvenir</p>
-                <p style="font-family: 'poppins'; font-size: 2rem;">typical of Gresik</p>
-            </section>
-        </div>
+    </div>
+    <div class="container">
+        <section id="landingpage" class="text-center">
+            <p style="font-size: 6rem;">Center souvenir</p>
+            <p style="font-family: 'poppins'; font-size: 2rem;">typical of Gresik</p>
+        </section>
+    </div>
     </div>
 
     <!-- options -->
